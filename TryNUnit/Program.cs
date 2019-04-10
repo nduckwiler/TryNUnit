25e482bb75c55981a8d68dd56341ee011e504687
Nick Duckwiler <nduckwiler@gmail.com>
2019-04-10 16:31:08 -0400

    Init repo with one broken test

---
+++ TryNUnit/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace TryNUnit
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static string FriendlyMainClaim()
+        {
+            return "I swear I'm nice!";
+        }
+
+        static string InternalFriendlyMainClaim()
+        {
+            return "Psst I'm nice";
+        }
+    }
+
+    public class Stomach
+    {
+        public string Growl()
+        {
+            return "grrr";
+        }
+    }
+}
---
+++ TryNUnit/ProgramTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace TryNUnit
+{
+    [TestFixture]
+    public class MainTest
+    {
+        // FriendlyMainClaim() must be public in this case
+        // MainClass has `internal` accessibility by default
+        [Test]
+        public void FriendlyMainClaimReturnsString()
+        {
+            string text = MainClass.FriendlyMainClaim();
+            Assert.That(text, Is.TypeOf(typeof(string)));
+        }
+
+        // InternalFriendlyMainClaim() is `internal` by default
+        [Test]
+        [Ignore("This test is not supposed to pass")]
+        public void InternalFriendlyMainClaimReturnsString()
+        {
+            string text = MainClass.InternalFriendlyMainClaim();
+            Assert.That(text, Is.TypeOf(typeof(string)));
+        }
+    }
+
+    [TestFixture]
+    public class StomachTest
+    {
+        [Test]
+        public void GrowlReturnsString()
+        {
+            Stomach organ = new Stomach();
+            string noise = organ.Growl();
+            Assert.That(noise, Is.InstanceOf(typeof(string)), "Expected Growl method to return a string");
+        }
+
+        [Test]
+        public void GrowlReturnsSpecificString()
+        {
+            Stomach organ = new Stomach();
+            string noise = organ.Growl();
+            Assert.That(noise, Is.EqualTo("grrr"), "Expected Growl method to return 'grrr'");
+        }
+    }
+}
